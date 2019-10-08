1097cd3da896ea39ebffdcb62b9eb6844fd640be
Carnivorous <38839485+carnivoroussociety@users.noreply.github.com>
2019-10-08 10:51:30 +0000

    Updated Dolphin Version to 10960

--- games/serious.c
+++ games/serious.c
@@ -17,7 +17,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, visit http://www.gnu.org/licenses/gpl-2.0.html
 //==========================================================================
-#include <stdio.h>
 #include <stdint.h>
 #include "../main.h"
 #include "../memory.h"
